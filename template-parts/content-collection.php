2876ef089908900547dc75cff856190667c48ec3
aliya robinson <aliya robinson>
2016-10-04 21:27:18 -0400

    currently only showing three of five posts

--- template-parts/content-collection.php
+++ template-parts/content-collection.php
@@ -11,7 +11,7 @@
 <?php $i = 0; ?>
 <?php //$i = 2; ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(['category','non-homepage']); ?>>
-	<div class="wrapper">
+  <div class="wrapper">
       <div class="grid-module">
         <header class="grid-header">
           <div class="grid-item large cat-hero">
@@ -24,122 +24,116 @@
             </blockquote>
           </div>
           <div class="side-items">
-          	<?php 
-      				$posts = get_field('items');
+            <?php 
+              $posts = get_field('items');
 
-      				if( $posts ): ?>
-      				    <ul>
-      				    <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
-      	        			<?php $i++; ?>
+              if( $posts ): ?>
+                  <ul>
+                  <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+                      <?php $i++; ?>
 
-      				        <?php setup_postdata($post); ?>
-      				        	<?php if($i<=2){ ?>
-      				    			<div class="grid-item small">
-      					        		<?php //echo "i: ". $i ?>
-      						            <img src="<?php the_post_thumbnail_url(); ?>" alt="">
-      						            <div class="hover-content">
-      						              <h2 class="title"><?php the_title(); ?></h2>
-      						              <p class="desc">Oil, Canvas, Acrylic. 2016</p>
-      						              <a href="<?php the_permalink(); ?>" class="view fa fa-search icon"></a>
-      						              <div class="share-mod">
-      						                <ul class="social-icons">
-      						                  <li class="social-item">
-      						                    <a href="#">
-      						                      <span aria-hidden="true" class="icon fa-facebook"></span>
-      						                      <span class="hide-copy" aria-label="Share on Facebook">  
-      						                        Facebook
-      						                      </span>
-      						                    </a>
-      						                  </li>
-      						                  <li class="social-item">
-      						                    <a href="#">
-      						                      <span aria-hidden="true" class="icon fa-twitter"></span>
-      						                      <span class="hide-copy" aria-label="Tweet">  
-      						                        Twitter
-      						                      </span>
-      						                    </a>
-      						                  </li>
-      						                  <li class="social-item">
-      						                    <a href="#"> 
-      						                      <span aria-hidden="true" class="icon fa-pinterest-p"></span>
-      						                      <span class="hide-copy" aria-label="Pin It">  
-      						                        Pinterest
-      						                      </span>
-      						                    </a>
-      						                  </li>
-      						                </ul>
-      						              </div>
-      						            </div>
-      						        </div>
-      				        	<?php } ?>
-      				        
-      				    <?php endforeach; ?>
-      				    </ul>
-      				    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-      			<?php endif; ?>
-
+                      <?php setup_postdata($post); ?>
+                        <?php if($i<=2){ ?>
+                        <div class="grid-item small">
+                            <?php //echo "i: ". $i ?>
+                              <img src="<?php the_post_thumbnail_url(); ?>" alt="">
+                              <div class="hover-content">
+                                <h2 class="title"><?php the_title(); ?></h2>
+                                <p class="desc">Oil, Canvas, Acrylic. 2016</p>
+                                <a href="<?php the_permalink(); ?>" class="view fa fa-search icon"></a>
+                                <div class="share-mod">
+                                  <ul class="social-icons">
+                                    <li class="social-item">
+                                      <a href="#">
+                                        <span aria-hidden="true" class="icon fa-facebook"></span>
+                                        <span class="hide-copy" aria-label="Share on Facebook">  
+                                          Facebook
+                                        </span>
+                                      </a>
+                                    </li>
+                                    <li class="social-item">
+                                      <a href="#">
+                                        <span aria-hidden="true" class="icon fa-twitter"></span>
+                                        <span class="hide-copy" aria-label="Tweet">  
+                                          Twitter
+                                        </span>
+                                      </a>
+                                    </li>
+                                    <li class="social-item">
+                                      <a href="#"> 
+                                        <span aria-hidden="true" class="icon fa-pinterest-p"></span>
+                                        <span class="hide-copy" aria-label="Pin It">  
+                                          Pinterest
+                                        </span>
+                                      </a>
+                                    </li>
+                                  </ul>
+                                </div>
+                              </div>
+                          </div>
+                        <?php } ?>
+                      
+                  <?php endforeach; ?>
+                  </ul>
+                  <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+            <?php endif; ?>
           </div>
         </header>
         <div class="grid">
 
-        <?php if(get_field('items')): $i = 0; while(has_sub_field('slides')): $i++; if ($i != 1): ?>
+        <?php if(get_field('items')): $i = 0; while(has_sub_field('items')): $i++; if ($i != 1): ?>
           Blah Blah Blah
         <?php endif; endwhile; endif; ?>
 
 
 
         <?php 
-    			$posts = get_field('items');
+          $posts = get_field('items');
 
-    			if( $posts ): ?>
-    			    <ul>
-    			    <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
-            			<?php $i++; ?>
-
-    			        <?php setup_postdata($post); ?>
-    				        <?php if($i>2){ ?>
-    			    			<div class="grid-item small">
-    					            <img src="<?php the_post_thumbnail_url(); ?>" alt="">
-    					            <div class="hover-content">
-    					              <h2 class="title"><?php the_title(); ?></h2>
-    					              <p class="desc"><?php the_excerpt(); ?></p>
-    					              <a href="<?php the_permalink(); ?>" class="view fa fa-search icon"></a>
-    					              <div class="share-mod">
-    					                <ul class="social-icons">
-    					                  <li class="social-item">
-    					                    <a href="#">
-    					                      <span aria-hidden="true" class="icon fa-facebook"></span>
-    					                      <span class="hide-copy" aria-label="Share on Facebook">  
-    					                        Facebook
-    					                      </span>
-    					                    </a>
-    					                  </li>
-    					                  <li class="social-item">
-    					                    <a href="#">
-    					                      <span aria-hidden="true" class="icon fa-twitter"></span>
-    					                      <span class="hide-copy" aria-label="Tweet">  
-    					                        Twitter
-    					                      </span>
-    					                    </a>
-    					                  </li>
-    					                  <li class="social-item">
-    					                    <a href="#"> 
-    					                      <span aria-hidden="true" class="icon fa-pinterest-p"></span>
-    					                      <span class="hide-copy" aria-label="Pin It">  
-    					                        Pinterest
-    					                      </span>
-    					                    </a>
-    					                  </li>
-    					                </ul>
-    					              </div>
-    					            </div>
-    					        </div>
-    				        <?php } ?>
-    			        
-    			    <?php endforeach; ?>
-    			    </ul>
-    			    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-    		<?php endif; ?>
+          if( $posts ): ?>
+              <ul>
+              <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+                  <?php setup_postdata($post); ?>
+                    <div class="grid-item small">
+                          <img src="<?php the_post_thumbnail_url(); ?>" alt="">
+                          <div class="hover-content">
+                            <h2 class="title"><?php the_title(); ?></h2>
+                            <p class="desc"><?php the_excerpt(); ?></p>
+                            <a href="<?php the_permalink(); ?>" class="view fa fa-search icon"></a>
+                            <div class="share-mod">
+                              <ul class="social-icons">
+                                <li class="social-item">
+                                  <a href="#">
+                                    <span aria-hidden="true" class="icon fa-facebook"></span>
+                                    <span class="hide-copy" aria-label="Share on Facebook">  
+                                      Facebook
+                                    </span>
+                                  </a>
+                                </li>
+                                <li class="social-item">
+                                  <a href="#">
+                                    <span aria-hidden="true" class="icon fa-twitter"></span>
+                                    <span class="hide-copy" aria-label="Tweet">  
+                                      Twitter
+                                    </span>
+                                  </a>
+                                </li>
+                                <li class="social-item">
+                                  <a href="#"> 
+                                    <span aria-hidden="true" class="icon fa-pinterest-p"></span>
+                                    <span class="hide-copy" aria-label="Pin It">  
+                                      Pinterest
+                                    </span>
+                                  </a>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                      </div>
+              <?php endforeach; ?>
+              </ul>
+              <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+        <?php endif; ?>
         </div>
       </div>
       <aside class="sidebar">
